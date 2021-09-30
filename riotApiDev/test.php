ff5057d9357fdb166f71cae25b2114cb373a461e
wilfried-klein <wilfried.klein@etu.umontpellier.fr>
2021-09-30 23:10:29 +0200

    modif classe api et ajout info champs

--- riotApiDev/test.php
+++ riotApiDev/test.php
@@ -1,8 +1,12 @@
 <?php 
-	require_once 'api.php';
-	$api = new Api('euw1','PΔblo');
-	$puuid = $api->getPuuidOfSummoner();
-	$matchId = $api->getLastMatchIDbyPuuid($puuid);
-	$playerdata = $api->getMatchStats($matchId);
-	print_r($playerdata);
+	require_once 'Api.php';
+	$summoner_name = 'PΔblo';
+	//renvoie le puuid de l'invocateur
+	//echo Api::getPuuidOfSummoner($summoner_name);
+	//renvoie les données de l'invocateur de son dernier match
+	//print_f(Api::getLastMatchStatsbySummoner($summoner_name));
+	//renvoie le nom du champions en fonctions de son ID
+	//echo Api::getChampionsNameByChampionsId(201);
+	//renvoie les données du champions suivant son Nom (avec maj)
+	//print_r(Api::getChampionDatas('Jhin'));
 ?>
