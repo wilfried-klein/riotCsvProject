3564c645600c7e5dbc083830ce2322910cc74826
wilfried-klein <wilfried.klein@etu.umontpellier.fr>
2021-10-01 22:07:46 +0200

    ajout affichage et formulaire

---
+++ riotApiDev/formulaire.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inscrivez votre nom d'invocateur</title>
+</head>
+<body>
+	<h2>Affiche vos statistiques de votre dernier match</h2>
+	<form method="post" action="display.php">
+		<fieldset>
+			<p>
+				<label for="summonerName">Nom d'invocateur </label> :
+				<input type="text" placeholder="Faker" name="summonerName" id="summonerName_id" required/>
+				
+			</p>
+		</fieldset>
+	</form>
+</body>
+</html>
