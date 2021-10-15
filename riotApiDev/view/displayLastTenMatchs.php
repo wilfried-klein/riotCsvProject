b6ed436b2480312102fc2790f3c6a0c7581448b0
Romain-Bessonnaud <romain.bessonnaud@etu.umontpellier.fr>
2021-10-15 18:42:13 +0200

    ajout barre de recherche dans le header

--- riotApiDev/view/displayLastTenMatchs.php
+++ riotApiDev/view/displayLastTenMatchs.php
@@ -12,6 +12,27 @@
 <body>
     <header>
         <img src="../controller/img/logo_lol.png" alt="logo de lol">
+        <form method="post" action="../controller/controllerLast10GamesInfo.php">
+            <fieldset>
+                <label for="summonerName_id"></label>
+                <input type="text" placeholder="ex: Faker" name="summonerName" id="summonerName_id" required/>
+                <label for="server_id"> </label>
+                <select name="server" id="server_id">
+                    <option value="br1">BR</option>
+                    <option value="eun1">EUNE</option>
+                    <option value="euw1" selected>EUW</option>
+                    <option value="jp1">JP</option>
+                    <option value="kr">KR</option>
+                    <option value="la1">LAN</option>
+                    <option value="la2">LAS</option>
+                    <option value="na1">NA</option>
+                    <option value="oc1">OCE</option>
+                    <option value="ru">RU</option>
+                    <option value="tr1">TR</option>
+                </select>
+            </fieldset>
+            <input type="submit" value="Envoyer" class="bouton">
+        </form>
         <div id="bordureLogo"></div>
     </header>
     <main>
