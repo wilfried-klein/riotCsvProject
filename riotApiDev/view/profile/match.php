7ff5daae78091935570bd55c2d31655b06b8ac4f
Romain-Bessonnaud <romain.bessonnaud@etu.umontpellier.fr>
2021-10-18 21:05:31 +0200

    début css pour table

--- riotApiDev/view/profile/match.php
+++ riotApiDev/view/profile/match.php
@@ -1,5 +1,5 @@
 <tr>
-	<td>
+	<td class="info1">
 		<?php
 		if($value['result'] == true){
 			echo '<h3 style="color: #1217C0;">VICTOIRE</h3>';
@@ -7,30 +7,37 @@
 			echo '<h3 style="color: #C01712;">DÉFAITE</h3>';
 		}
 		?>
-		<div>
-			<img src="<?php echo $value['championIcon']?>" alt="<?php echo $value['championName'] ?>"/>
-			<div>
-				<img src="<?php echo $value['summonerSpell1']?>"/>
-				<img src="<?php echo $value['summonerSpell2']?>"/>
-			</div>
-		</div>
-		<div>
-			<?php echo $value['championName'];?>
-			<img src="<?php echo $value['rune1Icon']?>"/>
-			<img src="<?php echo $value['rune2Icon']?>"/>
-		</div>
+        <div class="info1Global">
+            <div class="champEtSort">
+                <div class="champ">
+                    <img src="<?php echo $value['championIcon']?>" alt="<?php echo $value['championName'] ?>"/>
+                    <p><?php echo $value['championName'];?></p>
+                </div>
+                <div class="summonerSpell">
+                    <img src="<?php echo $value['summonerSpell1']?>"/>
+                    <img src="<?php echo $value['summonerSpell2']?>"/>
+                </div>
+            </div>
+            <div class="runes">
+                <img src="<?php echo $value['rune1Icon']?>"/>
+                <img src="<?php echo $value['rune2Icon']?>"/>
+            </div>
+        </div>
 	</td>
-	<td>
+
+	<td class="info2">
 		<p> <?php echo $value['gameDurationMin'] . " Min " . $value['gameDurationSeconde']?> </p>
 		<p><?php echo $value['matchType'] ?></p>
 		<p> Le <?php echo $value['matchDate']['mday'] . "/" . $value['matchDate']['mon'] . "/" . $value['matchDate']['year']?></p>
 	</td>
-	<td>
+
+	<td class="info3">
 		<p><?php echo $value['kills'] . "/" . $value['deaths'] . "/" . $value['assists'] ?></p>
 		<p><?php echo $value['creepScore']?> Cs</p>
 		<p>Score de vision : <?php echo $value['visionScore'] ?></p>
 	</td>
-	<td>
+
+	<td class="info4">
 		<?php
 		for ($i=0; $i < 7; $i++) {
 			if(array_key_exists($i,$value['itemsIcon'])){
@@ -39,7 +46,8 @@
 		};
 		?>
 	</td>
-	<td>
+
+	<td class="info5">
 		<?php 
 		for ($g=0; $g < 5; $g++) {
 			echo "<div>";
