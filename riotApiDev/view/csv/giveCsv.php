36b38122dd84ec4cb5a03f8bcf8d88dc2d9d86e8
Romain-Bessonnaud <romain.bessonnaud@etu.umontpellier.fr>
2021-12-12 09:55:52 +0100

    modification nom

--- riotApiDev/view/csv/giveCsv.php
+++ riotApiDev/view/csv/giveCsv.php
@@ -1,5 +1,5 @@
 <?php
-header('Content-Type: text/csv Content-Disposition: attachment; filename="test.csv"');
+header('Content-Type: text/csv Content-Disposition: attachment; filename="export.csv"');
 header('Content-Disposition: attachment; filename="gameData.csv"');
 echo $csvContent;
 ?>
